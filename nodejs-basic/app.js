d5905ae0ba3a736b3e2ce52e334026ad5845adb8
zainalsalamun <zainalrtf@gmail.com>
2021-05-27 15:56:58 +0700

    init commit

---
+++ nodejs-basic/app.js
@@ -0,0 +1,8 @@
+// const server = new Server({
+//     host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com'
+// })
+
+const firstName = process[2];
+const lastName = process.argv[3];
+
+console.log(`Hello ${firstName} ${lastName}`);
---
+++ nodejs-basic/index.js
@@ -0,0 +1,31 @@
+//  const message = (name) => {
+//     console.log(`Hello ${name}`);
+//  }
+
+//  message('JavaScript');
+
+// const greeting = function(name, language) {
+//    if(language ==="English") {
+//       return "Good Moring " + name + "!";
+//    } else if (language ==="French") {
+//       return "Bonjour " + name + "!";
+//    } else {
+//       return "Selamat Pagi " + name + "!";
+//    }
+// }
+
+// console.log(greeting('Ron','French'))
+
+const initialMemoryUsage = 10
+const yourName = "Zainal";
+const environMent = "";
+
+for (let i = 0; i <= 1000; i++) {
+
+}
+
+const currentMemoryUsage
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`)
+console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
