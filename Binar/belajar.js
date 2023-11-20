796abd907d0d47eee3a18639eaddb950cb009e4c
Zainal Salamun <zainalrtf@gmail.com>
2023-11-20 19:18:31 +0700

    add belajar js

---
+++ Binar/belajar.js
@@ -0,0 +1,24 @@
+console.log('hello world');
+
+const dataDiri = {
+    name: 'Viona',
+    age: 21,
+    alamat : 'Jl. Pahlawan'
+}
+
+dataDiri.pekerjaan = 'Programmer';
+console.log(dataDiri)
+
+const pekerjaan = ['guru','programmer','koki']
+console.log(pekerjaan)
+console.log(pekerjaan[0])
+const name ='Viona';
+const age = 21;
+const alamat2 ='Jl. Pahlawan'
+const pekerjaan2 = 'guru'
+const alamat ='Yogyakarta'
+const hasil = `Halo nama aku ${name}, umur ${age}, tinggal di ${alamat}, pekerjaan ${pekerjaan2}`
+console.log(pekerjaan2)
+console.log(hasil)
+
+
