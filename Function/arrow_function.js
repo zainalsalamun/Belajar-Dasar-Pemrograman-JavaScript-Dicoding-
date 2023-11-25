b1dca37ba146d7722d2a54b03a9b74d7bba9a083
Zainal Salamun <zainalrtf@gmail.com>
2023-11-25 08:33:09 +0700

    belajar arrow function

---
+++ Function/arrow_function.js
@@ -0,0 +1,39 @@
+function greet() {
+    console.log('hello');
+}
+
+const testArrow = () => {
+return 2+2
+}
+
+console.log(testArrow())
+
+
+function cobaFunction() {
+    return
+}
+
+// const hello = x => x
+// function hello(x) {
+//     return x
+// }
+// console.log(hello(1), hello(2))
+
+// const obj = {
+//     x1: 1,
+//     x2: 2,
+// }
+
+// const hello = ({x1, x2}) => {
+//     return x1 + x2
+// }
+
+// console.log(hello({x1: 1, x2: 2}))
+
+const obj = {
+    x1: [1, 2, 3],
+    x2: 2,
+    x3: 3,
+}
+
+console.log(obj.x1)
